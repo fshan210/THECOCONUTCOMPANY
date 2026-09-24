6d55b756ca719ba350b5606600997eebff35c3a2
Fazil Shersha <fazilshersha@Fazils-MacBook-Pro.local>
2026-09-24 15:56:01 +0530

    Serve Roboto fonts as WOFF2

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,10 +37,10 @@ import "../styles/reference-auth.css";
 
 const roboto = localFont({
   src: [
-    { path: "./fonts/Roboto-Light.ttf", weight: "300", style: "normal" },
-    { path: "./fonts/Roboto-Regular.ttf", weight: "400", style: "normal" },
-    { path: "./fonts/Roboto-Medium.ttf", weight: "500", style: "normal" },
-    { path: "./fonts/Roboto-Bold.ttf", weight: "700", style: "normal" }
+    { path: "./fonts/Roboto-Light.woff2", weight: "300", style: "normal" },
+    { path: "./fonts/Roboto-Regular.woff2", weight: "400", style: "normal" },
+    { path: "./fonts/Roboto-Medium.woff2", weight: "500", style: "normal" },
+    { path: "./fonts/Roboto-Bold.woff2", weight: "700", style: "normal" }
   ],
   variable: "--font-co-sans",
   display: "swap",
